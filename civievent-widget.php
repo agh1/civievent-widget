5abf411e79b3028008e8fc9f4eafec2bc0170214
agh1 <agh1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-06-14 20:36:14 +0000

    Revert "Merge remote-tracking branch 'origin/norepeatcity'"
    This reverts commit 957cbfaab6bdc4c705457c142580cf1233803321, reversing
    changes made to 318327161ad301353d7635f766e59794ee68a3aa.
    
    git-svn-id: https://plugins.svn.wordpress.org/civievent-widget/trunk@1893116 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- civievent-widget.php
+++ civievent-widget.php
@@ -56,6 +56,7 @@ add_action( 'widgets_init', function() {
  *                    	'custom' - use custom_display and custom_filter
  *                    - custom_display string JSON of custom display options (see documentation).
  *                    - custom_filter string JSON of custom filter options (see documentation).
+ *                    - event_type_id integer filter event listing using event type
  *                    All booleans default to false; any value makes them true.
  *
  * @return string The widget to drop into the post body.
@@ -135,6 +136,7 @@ class civievent_Widget extends WP_Widget {
 		'divider' => ', ',
 		'custom_display' => '',
 		'custom_filter' => '',
+		'event_type_id' => '',
 	);
 
 	/**
@@ -303,7 +305,9 @@ class civievent_Widget extends WP_Widget {
 
 		if ( $standardDisplay ) {
 			// Outputs the content of the widget.
-			$cal = CRM_Event_BAO_Event::getCompleteInfo();
+			// apply event type filter on standard output.
+			$event_type_id = !empty($instance['event_type_id']) ? $instance['event_type_id'] : NULL;
+			$cal = CRM_Event_BAO_Event::getCompleteInfo(NULL, $event_type_id);
 			$index = 0;
 			$content = '<div class="civievent-widget-list">';
 			foreach ( $cal as $event ) {
@@ -519,53 +523,34 @@ class civievent_Widget extends WP_Widget {
 	 * @param boolean $country Return country.
 	 */
 	public static function locationInfo( $eventId, $city = true, $state = null, $country = false ) {
+		$result = civicrm_api('Event', 'getsingle', array(
+			'version' => 3,
+			'id' => $eventId,
+			'is_show_location' => 1,
+			'return' => 'loc_block_id',
+			'api.LocBlock.getsingle' => array(
+				'id' => '$value.loc_block_id',
+				'api.Address.getsingle' => array( 'id' => '$value.address_id' ),
+			),
+		));
+
+		if ( ! empty( $result['is_error'] ) ) {
+			return array();
+		}
+
 		$return = array();
-
-		// Get the API names for each field we're potentially showing
-		$field_map = array(
-			'city' => 'city',
-			'state' => ( 'abbreviate' === $state ) ? 'state_province_id.abbreviation' : 'state_province_id.name',
-			'country' => 'country_id.name',
-		);
-
-		// Ignore the fields we don't need
-		foreach ( $field_map as $disp_field => $api_field ) {
-			if ( ! $$disp_field ) {
-				unset( $field_map[ $disp_field ] );
-			}
+		$loc = CRM_Utils_Array::value( 'api.Address.getsingle', CRM_Utils_Array::value( 'api.LocBlock.getsingle', $result, array() ), array() );
+		if ( $city ) {
+			$return['city'] = CRM_Utils_Array::value( 'city', $loc );
 		}
-
-		try {
-			$loc_block_id = civicrm_api3('Event', 'getvalue', array(
-				'return' => 'loc_block_id',
-				'id' => $eventId,
-				'is_show_location' => 1,
-			));
-
-			if ( empty( $loc_block_id ) ) {
-				return $return;
-			}
-
-			$address_id = civicrm_api3( 'LocBlock', 'getvalue', array(
-				'return' => 'address_id',
-				'id' => $loc_block_id,
-			));
-
-			if ( empty( $address_id ) ) {
-				return $return;
-			}
-
-			$loc = civicrm_api3( 'Address', 'getsingle', array(
-				'id' => $address_id,
-				'return' => array_values( $field_map ),
-			));
-
-		} catch (CiviCRM_API3_Exception $e) {
-			return $return;
+		if ( $state ) {
+			$abbreviate = ( 'abbreviate' === $state ) ? 'abbreviate' : null;
+			$states = CRM_Core_BAO_Address::buildOptions( 'state_province_id', $abbreviate, array( 'country_id' => CRM_Utils_Array::value( 'country_id', $loc ) ) );
+			$return['state'] = CRM_Utils_Array::value( CRM_Utils_Array::value( 'state_province_id', $loc ), $states );
 		}
-
-		foreach ( $field_map as $disp_field => $api_field ) {
-			$return[ $disp_field ] = $loc[ $api_field ];
+		if ( $country ) {
+			$countries = CRM_Core_BAO_Address::buildOptions( 'country_id', 'get' );
+			$return['country'] = CRM_Utils_Array::value( CRM_Utils_Array::value( 'country_id', $loc ), $countries );
 		}
 		return $return;
 	}
